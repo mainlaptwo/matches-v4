bac413fe43d0e3ee0d979e1b1780ccb9a990301c
mainlaptwo <106314714+mainlaptwo@users.noreply.github.com>
2022-06-01 12:43:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS,cross_origin
+import requests
+
+def getData():
+    url = requests.get("https://www.as-goal.com/mm/").text
+    x = url.find("<div id=\"Today\"")
+    y = url.find("<div id=\"Tomorrow\"")
+    return  url[x:y]
+
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route("/" , methods=['POST', 'GET','OPTIONS'])
+@cross_origin()
+def index():
+    return getData() , 200, {'Content-Type': 'text/html; charset=UTF-8'}
+#     return render_template("index.html", data=getData())
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
